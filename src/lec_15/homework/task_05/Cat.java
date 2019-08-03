43582619802bbe61e27535656e1be1a677b9f646
Berestovskij Dmitry <49187819+berdmival@users.noreply.github.com>
2019-08-03 18:34:11 +0300

    home work, task 5 ready

--- src/lec_15/homework/task_05/Cat.java
+++ src/lec_15/homework/task_05/Cat.java
@@ -1,9 +1,60 @@
 package lec_15.homework.task_05;
 
-public class Cat {
+import java.io.Serializable;
+
+public class Cat implements Serializable {
 	private String name;
 	private String breed;
 	private int age;
-	
-	
+
+	public Cat(String name, String breed, int age) {
+		this.name = name;
+		this.breed = breed;
+		this.age = age;
+	}
+
+	public Cat(String name, String breed) {
+		this.name = name;
+		this.breed = breed;
+		this.age = 0;
+	}
+
+	public Cat(String name) {
+		this.name = name;
+		this.breed = null;
+		this.age = 0;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Cat{" +
+				"name='" + name + '\'' +
+				", breed='" + breed + '\'' +
+				", age=" + age +
+				'}';
+	}
 }
--- src/lec_15/homework/task_05/Task_05.java
+++ src/lec_15/homework/task_05/Task_05.java
@@ -5,8 +5,50 @@ package lec_15.homework.task_05;
  * сериализовать лист в файл и десериализовать из файла. проверить наличие null элементов
  */
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task_05 {
-	public static void main (String[] args) {
-		List<Cat> kennel = new ArrayList<>();
+
+    private static final String SERIALIZED_LIST = "./src/lec_15/homework/task_05/serializedList";
+
+    public static void main(String[] args) {
+        List<Cat> kennel = new ArrayList<>();
+        kennel.add(new Cat("Vaska", "Maincun", 5));
+        kennel.add(null);
+        kennel.add(new Cat("Zuzia", "Sphinks"));
+        kennel.add(new Cat("Murka"));
+
+        System.out.println("kennel = " + kennel);
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(SERIALIZED_LIST);
+             ObjectOutputStream streamForSerializingList = new ObjectOutputStream(fileOutputStream)) {
+            streamForSerializingList.writeObject(kennel);
+            streamForSerializingList.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<Cat> secondKennel = new ArrayList<>();
+
+        try (FileInputStream fileInputStream = new FileInputStream(SERIALIZED_LIST);
+             ObjectInputStream streamForDeserializingList = new ObjectInputStream(fileInputStream)) {
+        	secondKennel = (List<Cat>) streamForDeserializingList.readObject();
+
+        	secondKennel.forEach(Cat -> {
+        		if (Cat == null) System.out.println("secondKennel contains null");
+			});
+
+			System.out.println("secondKennel = " + secondKennel);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
