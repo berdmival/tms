f1a06f92f9b45f84c964c3edd31ae747e0b1437a
berdmival <mtsby7049874@gmail.com>
2019-08-11 08:27:47 +0300

    some changes

---
+++ src/lec_17_2/homework/task_01/Task_01.java
@@ -0,0 +1,29 @@
+package lec_17_2.homework.task_01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task_01 {
+    private static final int FILL_LIMIT = 100;
+    public static void main(String[] args) {
+        List<Integer> listForFilling = new ArrayList<>();
+
+        ReentrantlockListFiller listFiller = new ReentrantlockListFiller(listForFilling, FILL_LIMIT);
+
+        Runnable runnable = () -> {
+            for (int i = 0; i < FILL_LIMIT; i++) {
+                listFiller.fillList();
+            }
+        };
+
+        Thread listFillerThread = new Thread(runnable);
+        listFillerThread.start();
+        try {
+            listFillerThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        listForFilling.forEach(listForFillingElement -> System.out.println(listForFillingElement));
+    }
+}
---
+++ src/lec_17_2/homework/task_03/FactorialCounter.java
@@ -0,0 +1,21 @@
+package lec_17_2.homework.task_03;
+
+import java.util.concurrent.Callable;
+
+public class FactorialCounter implements Callable<Long>{
+    private int numberForCountingFactorial;
+
+    public FactorialCounter(int numberForCountingFactorial) {
+        this.numberForCountingFactorial = numberForCountingFactorial;
+    }
+
+    @Override
+    public Long call() throws Exception {
+        Long factorial = 1L;
+
+        for (int i = 1; i < this.numberForCountingFactorial; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
