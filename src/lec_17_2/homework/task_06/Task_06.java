918835abc85d06a42166a4b7f4a04e6f1ac58f01
berdmival <mtsby7049874@gmail.com>
2019-08-14 16:05:50 +0300

    lec_17_2 task 6 is done

---
+++ src/lec_17_2/homework/task_06/Task_06.java
@@ -0,0 +1,24 @@
+package lec_17_2.homework.task_06;
+
+/*
+ * Task 6
+ * Написать программу, которая первым аргументом получает путь к папке, в которой создает 3 подпапки с именами "1", "2", "3"
+ */
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Task_06 {
+    public static void main(String[] args) {
+        String currentDirectory = args[0];
+
+        try {
+            Files.createDirectory(Paths.get(currentDirectory+"./1"));
+            Files.createDirectory(Paths.get(currentDirectory+"./2"));
+            Files.createDirectory(Paths.get(currentDirectory+"./3"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
