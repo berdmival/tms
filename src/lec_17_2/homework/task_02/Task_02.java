50df379b963c62da203d05a9dbc294c152f9fa28
berdmival <mtsby7049874@gmail.com>
2019-08-16 00:32:39 +0300

    fix lec_17_2 task_2

--- src/lec_17_2/homework/task_02/Task_02.java
+++ src/lec_17_2/homework/task_02/Task_02.java
@@ -1,8 +1,11 @@
 package lec_17_2.homework.task_02;
 
+import lec_17_2.homework.task_01.ReentrantlockListFiller;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
+import java.util.Random;
 
 public class Task_02 {
     private static final int FILL_LIMIT = 100;
@@ -10,11 +13,9 @@ public class Task_02 {
     public static void main(String[] args) {
         List<Integer> listForFilling = new ArrayList<>();
 
-       // ты мог просто переиспользовать класс из предыдущей задачи. Так было бы лучше + у тебя в этом же пакете еще один класс
         fillIntList(listForFilling);
 
-        //mapToInt(Integer::intValue)
-        OptionalDouble average = listForFilling.stream().mapToInt(e -> e).average();
+        OptionalDouble average = listForFilling.stream().mapToInt(Integer::intValue).average();
 
         System.out.println("average = " + average.getAsDouble());
     }
@@ -23,15 +24,32 @@ public class Task_02 {
         ReentrantlockListFiller listFiller = new ReentrantlockListFiller(listForFilling, FILL_LIMIT);
 
         Runnable runnable = () -> {
+            Random randomizer = new Random();
             for (int i = 0; i < FILL_LIMIT; i++) {
-                listFiller.fillList();
+                listFiller.fillList(randomizer.nextInt(i + 1));
             }
         };
 
-        Thread listFillerThread = new Thread(runnable);
-        listFillerThread.start();
+        Thread listFillerThread1 = new Thread(runnable);
+        listFillerThread1.start();
+        Thread listFillerThread2 = new Thread(runnable);
+        listFillerThread2.start();
+        Thread listFillerThread3 = new Thread(runnable);
+        listFillerThread3.start();
+        Thread listFillerThread4 = new Thread(runnable);
+        listFillerThread4.start();
+        Thread listFillerThread5 = new Thread(runnable);
+        listFillerThread5.start();
+        Thread listFillerThread6 = new Thread(runnable);
+        listFillerThread6.start();
+
         try {
-            listFillerThread.join();
+            listFillerThread1.join();
+            listFillerThread2.join();
+            listFillerThread3.join();
+            listFillerThread4.join();
+            listFillerThread5.join();
+            listFillerThread6.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
