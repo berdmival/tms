6463ea8dc080cbed47961e14121020e3cd69a53a
berdmival <mtsby7049874@gmail.com>
2019-08-15 19:26:55 +0300

    lec 15 task 8

--- src/lec_15/homework/task_08/MonkeyManager.java
+++ src/lec_15/homework/task_08/MonkeyManager.java
@@ -2,18 +2,35 @@ package lec_15.homework.task_08;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class MonkeyManager {
-    public int coconutMonkey(ArrayList<TreeBranch> branchForAnalysis) {
-        return 0;
+    public Integer coconutMonkey(TreeBranch branchForAnalysis) {
+        return calculateFruits(branchForAnalysis).get("coconuts");
     }
 
-    public int bananasMonkey(ArrayList<TreeBranch> branchForAnalysis) {
-        return 0;
+    public Integer bananasMonkey(TreeBranch branchForAnalysis) {
+        return calculateFruits(branchForAnalysis).get("bananas");
     }
 
-    public HashMap<String, Integer> coconutAndBananasMonkey(ArrayList<TreeBranch> branchForAnalysis) {
-        return new HashMap<>();
+    public HashMap<String, Integer> coconutAndBananasMonkey(TreeBranch branchForAnalysis) {
+        return calculateFruits(branchForAnalysis);
+    }
+
+    public HashMap<String, Integer> calculateFruits(TreeBranch branchForAnalysis) {
+        HashMap<String, Integer> hashMapOfCountsOfFruits = new HashMap<>();
+        hashMapOfCountsOfFruits.put("coconuts", 0);
+        hashMapOfCountsOfFruits.put("bananas", 0);
+        if (branchForAnalysis.getChildBranches().isEmpty()) {
+            hashMapOfCountsOfFruits.put("coconuts", hashMapOfCountsOfFruits.get("coconuts") + branchForAnalysis.getNumberOfCoconuts());
+            hashMapOfCountsOfFruits.put("bananas", hashMapOfCountsOfFruits.get("bananas") + branchForAnalysis.getNumberOfBananas());
+        } else {
+            ArrayList<TreeBranch> childBranches = branchForAnalysis.getChildBranches();
+            for (TreeBranch branch:childBranches) {
+                hashMapOfCountsOfFruits = calculateFruits(branch);
+                hashMapOfCountsOfFruits.put("coconuts", hashMapOfCountsOfFruits.get("coconuts") + branch.getNumberOfCoconuts());
+                hashMapOfCountsOfFruits.put("bananas", hashMapOfCountsOfFruits.get("bananas") + branch.getNumberOfBananas());
+            }
+        }
+        return hashMapOfCountsOfFruits;
     }
 }
